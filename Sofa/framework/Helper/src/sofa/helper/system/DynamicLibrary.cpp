dc887d05c8e55d4ea725b6457016d9aaf2de891b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-06-26 15:45:25 -0400

    [Helper] Update DynamicLibrary to support building using GCC v7
    This commit fixes the error reported below by copying and adapting
    compatibility logic already used in headers like PluginManager.cpp
    
    Error:
    
    [  1%] Building CXX object Sofa/framework/Helper/CMakeFiles/Sofa.Helper.dir/src/sofa/helper/system/DynamicLibrary.cpp.o
    /path/to/Slicer-SOFA-build/Sofa/Sofa/framework/Helper/src/sofa/helper/system/DynamicLibrary.cpp:31:10: fatal error: filesystem: No such file or directory
     #include <filesystem>
              ^~~~~~~~~~~~

--- Sofa/framework/Helper/src/sofa/helper/system/DynamicLibrary.cpp
+++ Sofa/framework/Helper/src/sofa/helper/system/DynamicLibrary.cpp
@@ -28,7 +28,16 @@ using sofa::helper::system::FileSystem;
 # include <dlfcn.h>
 #endif
 #include <string>
-#include <filesystem>
+#if __has_include(<filesystem>)
+  #include <filesystem>
+#elif __has_include(<experimental/filesystem>)
+  #include <experimental/filesystem>
+  namespace std {
+      namespace filesystem = experimental::filesystem;
+  }
+#else
+  error "Missing the <filesystem> header."
+#endif
 #include <sstream>
 
 namespace sofa::helper::system
